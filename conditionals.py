0b5bdc02f19ab1817d3710474a60622568d22e43
rohadisaidi78 <109731221+rohadisaidi78@users.noreply.github.com>
2024-02-23 19:43:59 +0700

    Update conditionals.py
    Terjemahkan ke bahasa Indonesia

--- conditionals.py
+++ conditionals.py
@@ -4,13 +4,13 @@ if userReply == "ya":
 else:
     print("Silakan datang kembali bila Anda ingin mengirimkan paket. Terima kasih.")
 #Working with Statement
-userReply = input("Would you like to buy stamps, buy an envelope, or make a copy? (Enter stamps, envelope, or copy) ")
-if userReply == "stamps":
-    print("We have many stamp designs to choose from.")
-elif userReply == "envelope":
-    print("We have many envelope sizes to choose from.")
-elif userReply == "copy":
-    copies = input("How many copies would you like? (Enter a number) ")
-    print("Here are {} copies.".format(copies))
+userReply = input("Apakah Anda mau membeli materai, amplop atau mau foto copy? (Ketik materai, amplop atau foto copy) ")
+if userReply == "materai":
+    print("Kami menyediakan beberapa design yang bisa Anda pilih.")
+elif userReply == "amplop":
+    print("Ada tersedia beberapa ukuran amplop.")
+elif userReply == "foto copy":
+    copies = input("Mau foto copy berapa kali? (Ketik angkanya) ")
+    print("Ini foto copynya rangkap {}.".format(copies))
 else:
-    print("Thank you, please come again.")
+    print("Terima kasih, silakan datang kembali.")
